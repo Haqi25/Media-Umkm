fb0a0880ef50117a91571f48f609670f6f31b3a9
Haqi25 <baihaqi1401@student.ub.ac.id>
2025-10-19 09:45:28 +0700

    fix: umkm function

--- src/routes/business.routes.js
+++ src/routes/business.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { store } from "../admin/controllers/category.controller.js"
-import { getNearby, searchUmkm, businessId, getAllBusiness, threeBusiness } from "../controllers/business.controller.js"
+import { getNearby, searchUmkm, businessId, getAllBusiness, threeBusiness, displayNewBusiness } from "../controllers/business.controller.js"
 const router = Router();
 
 router.get("/allBusiness", getAllBusiness)
@@ -9,5 +9,5 @@ router.post("/store", store)
 router.get("/nearby", getNearby)
 router.get("/:id", businessId )
 router.get("/display/landingPage", threeBusiness)
-
+router.get("/display/newUmkm", displayNewBusiness)
 export default router
