ccfe40cf798abec065b490afb532569ca5449070
Haqi25 <baihaqi1401@student.ub.ac.id>
2025-10-19 09:47:09 +0700

    fix:fetch data dashboard

--- src/admin/services/dashboard.services.js
+++ src/admin/services/dashboard.services.js
@@ -8,11 +8,7 @@ export const getDataDashboard = async() => {
     const userActive =await prisma.users.count({ where: {isVerified: true}})
     const review = await prisma.review.count()
 
-     const byCategory = await prisma.business.groupBy({
-      by: ["categoryId"],
-      _count: { id: true }
-    });
 
 
-    return {totalBusiness, userActive, review, byCategory}
+    return {totalBusiness, userActive, review,}
 }
